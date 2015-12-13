8c69f07f2d94d2b725c74dfb5a4ab5987070b750
Morton Fox <github@qslw.com>
2015-12-13 00:33:59 -0500

    Add truncate func

--- generate.rb
+++ generate.rb
@@ -2,7 +2,20 @@ require_relative 'config'
 
 module IcachingNuvi
   class Generate
+    # Maximum length of description, hints, and logs.
+    TEXT_LIMIT = 16_500
+
     def initialize
+    end
+
+    # Truncate a string to the specified length but clean up HTML entities at
+    # the end of the string that may have gotten chopped up.
+    def truncate s, len
+      return s if s.size < len
+      s = s[0, len]
+      # Clean up the end of the string so we don't leave a piece of a HTML
+      # entity behind when we truncate the string.
+      s[0...-7] + s[-7..-1].delete('&')
     end
 
     def smart_name str, maxlen
@@ -23,4 +36,5 @@ if __FILE__ == $PROGRAM_NAME
   s = ARGV.join ' '
   puts s
   puts IcachingNuvi::Generate.new.smart_name(s, 8)
+  puts IcachingNuvi::Generate.new.truncate(s, 8)
 end
