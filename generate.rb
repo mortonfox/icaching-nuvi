df257c19d8b4eed2f1848cc30442f70b51e9d98c
Morton Fox <github@qslw.com>
2015-12-14 03:22:04 -0500

    If the string begins with a common word, smart name should drop it.

--- generate.rb
+++ generate.rb
@@ -18,15 +18,26 @@ module IcachingNuvi
       s[0...-7] + s[-7..-1].delete('&')
     end
 
+    DISCARD_WORDS = %w(this that the a an)
+
     def smart_name str, maxlen
       words = str.scan(/\w+/)
+
+      # If the string begins with one of the words in DISCARD_WORDS, drop that word.
+      words.shift if words.size > 1 && DISCARD_WORDS.include?(words.first.downcase)
+
+      # Join words in camel case.
       squished = words.map(&:capitalize).join('')
+
+      # If there is a number, try to keep the least significant digits of it.
       match = squished.match(/^(\D*)(\d+)(.*)$/)
       if match
         digit_length = [match[2].size, 3].min
         prefix = match[1][0, maxlen - digit_length]
         squished = prefix + match[2][[match[2].size - (maxlen - prefix.size), 0].max..-1] + match[3]
       end
+
+      # Shorten string.
       squished[0, maxlen]
     end
   end
